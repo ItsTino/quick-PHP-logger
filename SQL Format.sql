2295dbdc2b8d8818d9c3a8590bd4b9fbeb997a31
Valentino <duvalvalentino@gmail.com>
2021-01-27 15:42:24 +0000

    Create SQL Format.sql
    SQL Code to create the used database table with fields

---
+++ SQL Format.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `development`.`eventlog` (
+    `eventID` INT NOT NULL AUTO_INCREMENT,
+    `eventTime` VARCHAR(512) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `eventType` VARCHAR(256) NOT NULL,
+    `eventInitiator` VARCHAR(512) NOT NULL,
+    `eventDetails` VARCHAR(1024) NOT NULL,
+    PRIMARY KEY (`eventID`)
+) ENGINE = InnoDB;
